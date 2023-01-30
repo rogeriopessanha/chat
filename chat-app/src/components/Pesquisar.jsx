2d789254edaa438c040f03bb8289363369c1e435
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-30 20:50:03 -0300

    att

--- chat-app/src/components/Pesquisar.jsx
+++ chat-app/src/components/Pesquisar.jsx
@@ -83,7 +83,8 @@ const Pesquisar = () => {
         <input
           type="text"
           placeholder="Procure seus contatos"
-          onKeyDown={handleKey}
+          // onKeyDown={handleKey}
+          onKeyUp={handleKey}
           onChange={(e) => setUsername(e.target.value)}
           value={username}
         />
